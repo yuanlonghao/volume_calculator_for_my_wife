c5ee29b60a24aae07ea7e118873bad50ba4f04f0
yuanlonghao <yuanlonghao1013@gmail.com>
2022-09-04 17:17:39 +0900

    enlarge result fonts

--- app.py
+++ app.py
@@ -9,4 +9,4 @@ height = st.number_input("高 (厘米)", value=0, min_value=0, step=1)
 n_package = st.number_input("箱子个数", value=0, min_value=0, step=1)
 
 result = length * width * height * n_package / 1000000 
-st.write(f"{n_package}个箱子的总体积是{result:.2f}立方米。")
+st.markdown(f"## {n_package}个箱子的总体积是**{result:.2f}**立方米。")
