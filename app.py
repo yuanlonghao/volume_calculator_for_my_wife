a6387b7c6f4289ff6ca67641bd234cc9a8984937
yuanlonghao <yuanlonghao1013@gmail.com>
2022-09-04 16:59:58 +0900

    add div

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.title('计算箱子总体积')
+
+length = st.number_input("长 (厘米)")
+width = st.number_input("宽 (厘米)")
+height = st.number_input("高 (厘米)")
+
+n_package = st.number_input("箱子个数")
+
+clicked = st.button("计算总体积")
